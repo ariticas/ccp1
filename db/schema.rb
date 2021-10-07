c2505a6215787ce238e8378f317554c31f4e5552
ariticas10 <ariticas10@gmail.com>
2021-10-07 15:27:25 -0700

    hello

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,30 @@
 
 ActiveRecord::Schema.define(version: 2021_10_07_221253) do
 
+  create_table "newcases", force: :cascade do |t|
+    t.string "state"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "dob"
+    t.string "email"
+    t.string "telephone_number"
+    t.string "addr"
+    t.string "respondent_fname"
+    t.string "respondent_lname"
+    t.string "respondent_addr"
+    t.string "marriage_date"
+    t.string "state_of_marriage"
+    t.string "date_of_seperation"
+    t.string "number_of_children"
+    t.string "children_addr"
+    t.string "occupation"
+    t.string "work_addr"
+    t.string "net_monthly"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "domesticp"
+    t.string "married", default: "no"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
