6df44090c2195562eb58d4bc498e252625ab6daf
ariticas10 <ariticas10@gmail.com>
2021-10-11 18:04:14 -0700

    hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:state, :number_of_children])
+    end
+
+
+
+
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :email, :state, :number_of_children ) }
+        devise_parameter_sanitizer.for(:account_update) { |u|    u.permit( :email,  :state, :number_of_children) }
+      end
+     
+  end
+  
